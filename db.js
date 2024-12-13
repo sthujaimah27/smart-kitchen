07b6d0b417bd22396cdaeb5ba7488e7205346bce
sthujaimah27 <sthujaimah27@gmail.com>
2024-12-13 20:08:10 +0700

    init backend

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: 'localhost', // Update with your MySQL host
-    user: 'root', // Update with your MySQL username
-    password: '', // Update with your MySQL password
-    database: 'smartkitchen_db' // The name of the database you created
+    host: process.env.HOST_DB, // Update with your MySQL host
+    user: process.env.USER_DB, // Update with your MySQL username
+    password: process.env.PASS_DB, // Update with your MySQL password
+    database: process.env.NAME_DB // The name of the database you created
 });
 
 module.exports = pool.promise();
